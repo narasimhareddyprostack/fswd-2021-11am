a99fe8e82022bed808cfeb1d22303fad0a36e0e9
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-01-23 12:22:27 +0530

    'hello'

---
+++ JS4/Basics/Array1/one.js
@@ -0,0 +1,14 @@
+let size = [38,40,42,44]
+
+console.log(size);
+
+console.log(size[0]);
+
+console.log(size[size.length - 1]);
+
+let emp_Names = ['imaran', 'chiru', 'ashish', 'yaman','sidd', 'pawan','ganesh', 'aa'];
+
+
+console.log(emp_Names);
+
+console.log(emp_Names[0]);

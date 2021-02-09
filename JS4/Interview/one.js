dbcad3e2493e223743e14685d1636581a6c6148a
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-02-09 12:26:17 +0530

    okie

---
+++ JS4/Interview/one.js
@@ -0,0 +1,6 @@
+let employee_Names = ["ganesh", "chiru", "siddiq", "arjun", "imran"];
+console.log(employee_Names.map((emp) => emp.toLocaleUpperCase()));
+
+for (let emp of employee_Names) {
+  console.log(emp.toLocaleUpperCase());
+}
